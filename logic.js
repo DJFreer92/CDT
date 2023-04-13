1135bc749a982852db35495cedaf49fbae69f320
Daniel Freer <djfreer@icloud.com>
2023-04-13 15:54:23 -0400

    made checkData function and removed progress bar

--- logic.js
+++ logic.js
@@ -1,4 +1,4 @@
-
+//document.getElementById("genderText").textContent = "TEST";
 /*makes the phone number in the phone number textbox more readable to the user e.g. (123) 456-7890
  *this function is called by the phone number input element when it is unfocused by the user
  */
@@ -43,6 +43,30 @@ function getData(form) {
 	//traverses the form data
 	for (let pair of formData.entries()) {
 		console.log(pair[0] + ": " + pair[1]);
+		checkData(pair);
+	}
+}
+
+//checks the data from the form
+function checkData(data) {
+	switch (data[0]) {
+		case "firstName": break;
+		case "lastName": break;
+		case "gender": break;
+		case "age": break;
+		case "symptoms":
+			if (data[1] == "cough") {
+
+			} else if (data[1] == "fever") {
+
+			} else if (data[1] == "lossOfTaste") {
+
+			} else if (data[1] == "lossOfSmell") {
+
+			}
+			break;
+		case "phone": break;
+		case "email": break;
 	}
 }
 
